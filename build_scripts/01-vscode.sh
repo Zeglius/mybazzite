a3a1dd23eb5573bf8b5e782b07fee56c998b9d9f
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-24 17:17:40 +0100

    fix (build_scripts): Dont use sudo in 01-vscode.sh
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/01-vscode.sh
+++ build_scripts/01-vscode.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S bash -euo pipefail
 
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |
-    sudo tee /etc/yum.repos.d/vscode.repo >/dev/null &&
+    tee /etc/yum.repos.d/vscode.repo >/dev/null &&
     dnf5 install -y --setopt=install_weak_deps=0 code
