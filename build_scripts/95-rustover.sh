e7a9c01d19293256aed045b84712a156637aa3d3
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-24 17:38:08 +0100

    fix (build_scripts): Add failsafe to Rustover
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/95-rustover.sh
+++ build_scripts/95-rustover.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env -S bash -euo pipefail
-curl https://download.jetbrains.com/rustrover/RustRover-2024.3.3.tar.gz | tar -xa -C "$(realpath /opt)"
+{
+    curl -sSLO https://download.jetbrains.com/rustrover/RustRover-2024.3.3.tar.gz | tar -xa -C "$(realpath /opt)"
+} || {
+    echo "::warning::Couldnt setup Rustover. Skipping..."
+    exit 0
+}
