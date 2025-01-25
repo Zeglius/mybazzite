3254c03202f928b9a332ff2f712257fddde763c5
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-25 13:44:40 +0100

    fix (rustover): Utilize factory dir to install
    Dump rustover tar into /usr/share/factory and symlink to /var/opt
    
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/05-rustover.sh
+++ build_scripts/05-rustover.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env -S bash -euo pipefail
-{
-    curl -sSLO https://download.jetbrains.com/rustrover/RustRover-2024.3.3.tar.gz | tar -xa -C "$(realpath /opt)"
-} || {
-    echo "::warning::Couldnt setup Rustover. Skipping..."
-    exit 0
+
+on_err() {
+	echo "::warning::Couldnt setup Rustover. Skipping..."
+	exit 0
 }
+
+trap 'on_err' ERR
+
+canon_dest=/var/opt/Rustover
+dest=/usr/share/factory/${canon_dest##/}
+mkdir -p "$dest"
+
+tmpdir=$(mktemp -d)
+trap 'rm -vrf $tmpdir' EXIT
+
+curl -sSLo "$tmpdir"/rustover.tar.gz https://download.jetbrains.com/rustrover/RustRover-2024.3.3.tar.gz
+tar -xaf "$tmpdir"/rustover.tar.gz -C "$dest" --strip-components=1
+
+# Write a tmpfile entry
+# See https://www.freedesktop.org/software/systemd/man/257/tmpfiles.d.html#L
+cat >/usr/lib/tmpfiles.d/rustover.conf <<EOF
+#Type  Path         Mode  User  Group  Age  Argument…
+L+     $canon_dest  -     -     -      -    $dest
+
+EOF
