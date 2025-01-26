da958d3abf4f8d0b38188fe3a10bc681ba98b37d
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-26 15:55:32 +0100

    feat (build_scripts): Add bind_state func
    Allow bind mounting stateful directories into /usr/share/factory
    
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/source.sh
+++ build_scripts/source.sh
@@ -15,3 +15,45 @@ skip_on_err() {
 
 export -f skip_on_err
 
+# Mount a bindfs in stateful dirs into /usr/share/factory.
+# This is useful to allow writting in /var/opt or /var/home.
+bind_state() {
+	local -r NULL=/dev/null
+	local -r FACTORY=/usr/share/factory
+	declare -r _stateful_dirs=(
+		/var/opt
+		/var/home
+	)
+
+	echo >&2 "Checking if bindfs is available"
+	if ! type -P bindfs >$NULL; then
+		echo >&2 "bindfs is not available. Installing..."
+		dnf5 install -yq bindfs
+	else
+		echo >&2 "bindfs found at $(type -P bindfs)"
+	fi
+
+	local dir
+	for dir in "${_stateful_dirs[@]}"; do
+		[[ -L $dir ]] && {
+			echo >&2 "::error::'$dir' must not be a symlink. Aborting..."
+			exit 1
+		}
+		[[ ! -e $dir ]] && mkdir -p "$dir"
+		[[ ! -e "$FACTORY/$dir" ]] && mkdir -p "$FACTORY/$dir"
+
+		bindfs -o allow_other "$FACTORY/$dir" "$dir"
+	done
+
+	# shellcheck disable=SC2317
+	unbind_state() {
+		unset -f unbind_state
+		local dir
+		for dir in "${_stateful_dirs[@]}"; do
+			umount --recursive "$dir"
+		done
+		unset -c _stateful_dirs
+	}
+}
+
+export -f bind_state
