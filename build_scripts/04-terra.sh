fe22d4d35654dacd792c7db516f05ed590714fde
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-03 21:32:10 +0100

    fix (terra): Add '--overwrite' flag to config-manager
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/04-terra.sh
+++ build_scripts/04-terra.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S bash -euo pipefail
-dnf5 config-manager addrepo -y --from-repofile=https://terra.fyralabs.com/terra.repo
+dnf5 config-manager addrepo --overwrite -y --from-repofile=https://terra.fyralabs.com/terra.repo
 dnf5 install -y --setopt=install_weak_deps=0 ghostty-nightly
