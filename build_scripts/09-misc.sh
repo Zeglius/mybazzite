af71a83247aa294591e5b2cf4b9de3b44def6902
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-05 16:11:52 +0100

    package : Add podman 5.4.0~rc2
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

---
+++ build_scripts/09-misc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S bash -euo pipefail
+
+set -x
+trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
+
+echo "::group::Setup podman testing"
+dnf5 -y --repo=updates-testing swap podman podman
+echo "::endgroup::Setup podman testing"
