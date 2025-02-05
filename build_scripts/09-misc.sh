073330836088634e8163c6ca013e0eed93347d21
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-05 18:06:28 +0100

    package : Disable podman rc
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-misc.sh
+++ build_scripts/09-misc.sh
@@ -3,6 +3,6 @@
 set -x
 trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
 
-echo "::group::Setup podman testing"
-dnf5 -y --repo=updates-testing swap podman podman
-echo "::endgroup::"
+#echo "::group::Setup podman testing"
+#dnf5 -y --repo=updates-testing swap podman podman
+#echo "::endgroup::"
