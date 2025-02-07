347f21718111889f2fc273802d937d7ccdfffcb1
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-07 17:04:44 +0100

    fix (brew): Simplify script catt
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-misc.sh
+++ build_scripts/09-misc.sh
@@ -14,7 +14,5 @@ trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
 	}
 	mkdir -p /var/home /var/roothome
 	export NONINTERACTIVE=1
-	curl -fsSLo /tmp/brew-install https://raw.githubusercontent.com/Zeglius/packages/refs/heads/brew-script/containerfile_scripts/brew.sh &&
-		chmod +x /tmp/brew-install &&
-		/tmp/brew-install
+	bash < <(curl -fsSL https://raw.githubusercontent.com/Zeglius/packages/refs/heads/brew-script/containerfile_scripts/brew.sh)
 } || echo "::warning::Failed to install: brew"
