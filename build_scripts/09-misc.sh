dd63b09b55ed1d99fa0ffc71269a413aeb761495
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-07 13:46:10 +0100

    fix (brew): Better installation
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-misc.sh
+++ build_scripts/09-misc.sh
@@ -7,6 +7,14 @@ trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
 #dnf5 -y --repo=updates-testing swap podman podman
 #echo "::endgroup::"
 
-export NONINTERACTIVE=1
-curl -fsSL https://raw.githubusercontent.com/Zeglius/packages/refs/heads/brew-script/containerfile_scripts/brew.sh |
-	bash || echo "::warning::Failed to install: brew"
+{
+	[[ ! -f /.dockerenv ]] && {
+		touch /.dockerenv
+		trap 'rm /.dockerenv' EXIT
+	}
+	mkdir -p /var/home /var/roothome
+	export NONINTERACTIVE=1
+	curl -fsSLo /tmp/brew-install https://raw.githubusercontent.com/Zeglius/packages/refs/heads/brew-script/containerfile_scripts/brew.sh &&
+		chmod +x /tmp/brew-install &&
+		/tmp/brew-install
+} || echo "::warning::Failed to install: brew"
