b3df8954e37569dcbbb897b306e57c6dccc79da5
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-11 01:07:44 +0100

    refactor(unity): Dont use bindfs
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/06-unity.sh
+++ build_scripts/06-unity.sh
@@ -5,12 +5,12 @@ trap 'skip_on_err "Error installing unityhub"' ERR
 canon_dest=/var/opt/unityhub
 dest=/usr/share/factory/${canon_dest##/}
 
-bind_state
+mkdir -p /var/opt /usr/share/factory/var/opt
 
 sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
 dnf5 install -y --setopt=install_weak_deps=0 unityhub
 
-unbind_state
+mv -T "$canon_dest" "$dest" 
 
 dnf5 config-manager setopt unityhub.enabled=0
 
@@ -18,6 +18,6 @@ dnf5 config-manager setopt unityhub.enabled=0
 # See https://www.freedesktop.org/software/systemd/man/257/tmpfiles.d.html#L
 cat >/usr/lib/tmpfiles.d/unityhub.conf <<EOF
 #Type  Path         Mode  User  Group  Age  Argument…
-L+     $canon_dest  -     -     -      -    $dest
+C+     $canon_dest  -     -     -      -    $dest
 
 EOF
