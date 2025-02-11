1f6cc21d6f9a2b62d5f9fa1e1374421148f723aa
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-11 13:43:19 +0100

    feat: Use updates-testing for latest plasma 6.3
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-misc.sh
+++ build_scripts/09-misc.sh
@@ -16,3 +16,8 @@ trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
 	export NONINTERACTIVE=1
 	bash < <(curl -fsSL https://raw.githubusercontent.com/Zeglius/packages/refs/heads/brew-script/containerfile_scripts/brew.sh)
 } || echo "::warning::Failed to install: brew"
+
+# kde plasma 6.3
+while read -r package; do
+	dnf5 -y swap --repo=updates-testing "$package" "$package"
+done < <(dnf5 repoquery --installed --queryformat='%{name}\n' "*plasma*")
