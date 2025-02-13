616495caf90ac3bb17ae01fe02c9042caac6fed9
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-02-13 17:38:40 +0100

    feat: Add gvproxy for podman machine
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-misc.sh
+++ build_scripts/09-misc.sh
@@ -16,3 +16,8 @@ trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
 	export NONINTERACTIVE=1
 	bash < <(curl -fsSL https://raw.githubusercontent.com/Zeglius/packages/refs/heads/brew-script/containerfile_scripts/brew.sh)
 } || echo "::warning::Failed to install: brew"
+
+{
+	dnf5 install -y --setopt=install_weak_deps=0 /usr/libexec/podman/gvproxy ||
+		echo "::warning::Failed to install: gvproxy"
+}
