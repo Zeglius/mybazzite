2fd85d7d2392f87c9f47e3bde73df0118f3fa13a
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-03-27 00:22:52 +0100

    feat: Add aero theme
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-misc.sh
+++ build_scripts/09-misc.sh
@@ -49,6 +49,54 @@ trap 'skip_on_err "Couldnt setup miscellanea stuff"' ERR
 	ln -s /usr/bin/rclone /sbin/mount.rclone || :
 }
 
+# Install windows aero theme
+# See https://gitgud.io/wackyideas/aerothemeplasma
+{
+	dnf5 -yq install \
+		plasma-workspace-devel \
+		kvantum \
+		qt6-qtmultimedia-devel \
+		qt6-qt5compat-devel \
+		libplasma-devel \
+		qt6-qtbase-devel \
+		qt6-qtwayland-devel \
+		plasma-activities-devel \
+		kf6-kpackage-devel \
+		kf6-kglobalaccel-devel \
+		qt6-qtsvg-devel \
+		wayland-devel \
+		plasma-wayland-protocols \
+		kf6-ksvg-devel \
+		kf6-kcrash-devel \
+		kf6-kguiaddons-devel \
+		kf6-kcmutils-devel \
+		kf6-kio-devel \
+		kdecoration-devel \
+		kf6-ki18n-devel \
+		kf6-knotifications-devel \
+		kf6-kirigami-devel \
+		kf6-kiconthemes-devel \
+		cmake \
+		gmp-ecm-devel \
+		kf5-plasma-devel \
+		libepoxy-devel \
+		kwin-devel \
+		kf6-karchive \
+		kf6-karchive-devel \
+		plasma-wayland-protocols-devel \
+		qt6-qtbase-private-devel \
+		qt6-qtbase-devel
+
+	pushd "${tmpdir:=$(mktemp -p)}"
+
+	git clone https://gitgud.io/wackyideas/aerothemeplasma.git .
+	sh compile.sh || :
+	chmod +x install_plasmoids.sh && ./install_plasmoids.sh
+	chmod +x install_plasma_components.sh && ./install_plasma_components.sh
+
+	popd && rm -rf "$tmpdir" && unset -v tmpdir
+}
+
 # Install drop-in packages
 {
 	for pkg in /tmp/packages/*.rpm; do
