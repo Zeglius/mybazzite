92e357caef1ed694fa01bb5a92d61b476b11929c
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-04-16 21:28:26 +0200

    fix: Fallback to docker-ce-testing in fedora 42
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/03-docker.sh
+++ build_scripts/03-docker.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env -S bash -euo pipefail
-dnf5 config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo &&
-	dnf5 install -y --setopt=install_weak_deps=0 \
-		docker-ce \
-		docker-ce-cli \
-		containerd.io \
-		docker-buildx-plugin \
-		docker-compose-plugin
+
+pkgs=(
+	docker-ce 
+	docker-ce-cli 
+	containerd.io 
+	docker-buildx-plugin 
+	docker-compose-plugin
+)
+
+dnf5 config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 install -y --setopt=install_weak_deps=0 "${pkgs[@]}" || {
+	# Use test packages if docker pkgs is not available for f42
+	if (( $(lsb_release -sr) == 42 )); then
+		echo "::info::Missing docker packages in f42, falling back to testing repos..."
+		dnf5 install -y --enablerepo=docker-ce-testing "${pkgs[@]}"
+	fi
+}
 
 dnf5 config-manager setopt docker-ce-stable.enabled=0
