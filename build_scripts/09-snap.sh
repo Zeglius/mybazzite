b9deab5eb45a4ad62254ad9757682e34f9b360d1
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-12-06 19:46:48 +0100

    fix: enable snap classic mode
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-snap.sh
+++ build_scripts/09-snap.sh
@@ -6,27 +6,8 @@ trap 'skip_on_err "Couldnt setup snap"' ERR
 dnf install -yq snapd
 
 # Ensure /snap directory exists and is writable
-mkdir -p /snap
-cat <<EOF >/usr/lib/systemd/system/snap.mount
-# Workaround for snapd in Fedora atomic bootc
-# 
-# This ensures we have a writable /snap directory
-
-[Unit]
-Description=Bind mount /var/snap on /snap
-Before=snapd.mounts-pre.target
-DefaultDependencies=no
-After=systemd-remount-fs.service
-Before=local-fs.target
-
-[Mount]
-What=/var/snap
-Where=/snap
-Options=bind,rw
-
-[Install]
-WantedBy=local-fs.target
-EOF
+rm -rf /snap
+ln -s /var/lib/snapd/snap /snap
 systemctl enable snap.mount
 
 cat >/usr/lib/tmpfiles.d/snap-bootc-writable.conf <<EOF
