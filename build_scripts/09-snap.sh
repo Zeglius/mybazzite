cfa2789feb5fe4d1d916c91193e7be15e3239deb
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-12-06 16:48:09 +0100

    fix: Better handling of a writable /snap mount
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_scripts/09-snap.sh
+++ build_scripts/09-snap.sh
@@ -5,10 +5,31 @@ trap 'skip_on_err "Couldnt setup snap"' ERR
 
 dnf install -yq snapd
 
-rm -rf /snap
-ln -sf /var/snap /snap
+# Ensure /snap directory exists and is writable
+mkdir -p /snap
+cat <<EOF >/usr/lib/systemd/system/snap.mount
+# Workaround for snapd in Fedora atomic bootc
+# 
+# This ensures we have a writable /snap directory
 
-cat >/usr/lib/tmpfiles.d/unityhub.conf <<EOF
+[Unit]
+Description=Bind mount /var/snap on /snap
+Before=snapd.mounts-pre.target
+DefaultDependencies=no
+After=systemd-remount-fs.service
+Before=local-fs.target
+
+[Mount]
+What=/var/snap
+Where=/snap
+Options=bind,rw
+
+[Install]
+WantedBy=local-fs.target
+EOF
+systemctl enable snap.mount
+
+cat >/usr/lib/tmpfiles.d/snap-bootc-writable.conf <<EOF
 #Type  Path         Mode  User  Group  Age  Argument…
 d      /var/snap   755   root  root   -
 EOF
